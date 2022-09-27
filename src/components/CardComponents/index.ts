e7db0c8509dd5d3df53553044b45633daeee3849
Renan-A-Lira <renanalmeida2019.ra@gmail.com>
2022-09-27 10:49:49 -0300

    imports resolve

---
+++ src/components/CardComponents/index.ts
@@ -0,0 +1,12 @@
+import Card from "./Card";
+import HeaderCard from "./HeaderCard";
+import FooterCard from "./FooterCard";
+import BodyCard from "./BodyCard";
+
+
+export {
+    Card,
+    HeaderCard,
+    FooterCard,
+    BodyCard
+}
